d425e39b2f762839cd5225348c04d4346617a9ac
Masoud Mir hosseini <sm2.masoud@gmail.com>
2021-10-13 09:47:57 +0330

    chore(tailwind):add packages & settup

--- craco.config.js
+++ craco.config.js
@@ -2,6 +2,11 @@ const CracoLessPlugin = require('craco-less');
 const CracoAlias = require('craco-alias');
 
 module.exports = {
+	style: {
+		postcss: {
+			plugins: [require('tailwindcss'), require('autoprefixer')],
+		},
+	},
 	plugins: [
 		{
 			plugin: CracoLessPlugin,
--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { Button } from 'antd';
 import React from 'react';
+import './App.less';
 
 const App = () => {
-	return <div className=''>start</div>;
+	return (
+    <>
+    <Button>ddd</Button>
+    
+		<button class='py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700'>
+			Click me
+		</button>
+    </>
+	);
 };
 export default App;
