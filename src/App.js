4047a02af5ade6ec75b9a5c790430b8b71bd495f
Masoud Mir hosseini <sm2.masoud@gmail.com>
2021-10-13 18:37:09 +0330

    feat(*):add fonts & create structure styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import { Button } from 'antd';
+import { Button, ConfigProvider } from 'antd';
 import React from 'react';
 import './App.less';
+import './Styles/main.scss';
 
 const App = () => {
 	return (
-    <>
-    <Button>ddd</Button>
-    
-		<button class='py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700'>
-			Click me
-		</button>
-    </>
+		<>
+			<ConfigProvider direction='rtl'>
+				<Button>ddd</Button>
+
+				<button class='py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700'>
+					Click me
+				</button>
+			</ConfigProvider>
+		</>
 	);
 };
 export default App;
