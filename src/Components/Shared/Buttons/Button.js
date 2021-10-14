d4860b1840e40402e6474e24cebbeb320ca29e45
Masoud Mir hosseini <sm2.masoud@gmail.com>
2021-10-14 18:33:41 +0330

    feat(Button):create button

---
+++ src/Components/Shared/Buttons/Button.js
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Button = ({ type, color, classes }) => {
+	return <div className=''></div>;
+};
+
+Button.propTypes = {
+	classes: PropTypes.string,
+	color: PropTypes.string,
+	type: PropTypes.string,
+};
+export default Button;
