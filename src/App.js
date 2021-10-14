91adcaad3fd71c5f93aed4afd7753b7283131f64
Saba Beigi <beigisaba@gmail.com>
2021-10-14 04:01:09 +0330

    feat footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import { Button } from 'antd';
-import React from 'react';
-import './App.less';
+import Footer from "@Components/Share/Footer";
+import "./App.less";
 
 const App = () => {
-	return (
-    <>
-    <Button>ddd</Button>
-    
-		<button class='py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700'>
-			Click me
-		</button>
-    </>
-	);
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
 };
 export default App;
