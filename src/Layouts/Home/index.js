82a255133f15b8053d89c0c8af0e5cb939ae4407
Masoud Mir hosseini <sm2.masoud@gmail.com>
2021-10-17 20:23:22 +0330

    feat(Title):create component

--- src/Layouts/Home/index.js
+++ src/Layouts/Home/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import Title from '@Components/Shared/Title';
 
 const Home = () => {
 	return (
 		<div className=''>
 			<Header />
+			<Title>ویژگی های کدآموز</Title>
 		</div>
 	);
 };
