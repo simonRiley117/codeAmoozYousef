2dc64d34e06a9ded95b157fe4d555df000203337
Saba Beigi <beigisaba@gmail.com>
2021-10-18 17:13:07 +0330

    feat coworker main page

---
+++ src/Layouts/CoWorkers/index.js
@@ -0,0 +1,75 @@
+import React from "react";
+import BreadCrump from "@Components/Shared/BreadCrump/BreadCrump";
+import Title from "@Components/Shared/Title";
+import master from "@Assets/Pic/master.png";
+import coworkerTeam from "@Assets/Pic/coworkerTeam.png";
+import cowrkercreater from "@Assets/Pic/cowrkercreater.png";
+import Button from "@Components/Shared/Buttons/Button";
+import { Link } from "react-router-dom";
+
+function index() {
+  return (
+    <div className="CoWorkers container">
+      <BreadCrump item={menu} />
+      <div className="CoWorkers__titleBox">
+        <Title>همکاری با ما</Title>
+      </div>
+      <div className="CoWorkers__contentBox w-4/5 grid grid-cols-3 justify-center items-center">
+        {info.map((index, id) => (
+          <div
+            className={`CoWorkers__box flex flex-col justify-between items-center CoWorkers__box-${id}`}
+            key={id}
+          >
+            <p className="CoWorkers__titr font-bold">{index.title}</p>
+
+            <img
+              src={index.icon}
+              alt={index.icon}
+              className="CoWorkers__icons"
+            />
+            <p className="CoWorkers__txt leading-loose">{index.txt}</p>
+
+            <div className="text-center">
+              <Button ico={false} type="primary" classes="CoWorkers__btn">
+                <Link to={index.rout}>همکاری با ما</Link>
+              </Button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default index;
+const menu = [
+  {
+    name: "صفحه اصلی",
+    rout: "",
+  },
+  {
+    name: "همکاران و اساتید",
+    rout: "coWorkers",
+  },
+];
+const info = [
+  {
+    title: "تیم کارفرما",
+    icon: cowrkercreater,
+    txt: "اگه شما شرکت یا استارت آپی دارید که دوست  دارید با ما همکاری کنید خوشحال میشیم که  باهاتون آشنا بشیم.",
+    rout: "/coWorkers/employer",
+  },
+
+  {
+    title: "تیم فنی",
+    icon: coworkerTeam,
+    txt: "اگه در زمینه های برنامه نویسی و یا شاخه های گرافیک فعالیت دارید می تونید با ما همکاری کنید.",
+    rout: "/coWorkers/team",
+  },
+  {
+    title: "اساتید",
+    icon: master,
+    txt: "اگه جزو اساتید باتجربه هستید و دوست دارید دوره هاتونو در اختیار دانشجو های علاقه مند قرار بدید وقتشه همکاری با ما رو شروع کنید.",
+    rout: "/coWorkers/master",
+  },
+];
--- src/Router/CoWorkersRouter.js
+++ src/Router/CoWorkersRouter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch, useRouteMatch } from "react-router-dom";
-import Master from "../Layouts/CoWorkers/Master/Master";
+import Master from "@Layouts/CoWorkers/Master/Master";
+import CoWorkers from "@Layouts/CoWorkers";
 
 const CoWorkersRouter = () => {
   let { path, url } = useRouteMatch();
@@ -8,6 +9,9 @@ const CoWorkersRouter = () => {
   return (
     <div>
       <Switch>
+        <Route exact path={`${path}`}>
+          <CoWorkers />
+        </Route>
         <Route exact path={`${path}/master`}>
           <Master />
         </Route>
