8b81135d0a94951028a7f4915f6ea51a1251c822
Saba Beigi <beigisaba@gmail.com>
2021-10-28 01:31:35 +0330

    style date in answer box

--- src/Components/Layouts/Course/Comment/AskAndAnswer.js
+++ src/Components/Layouts/Course/Comment/AskAndAnswer.js
@@ -8,6 +8,7 @@ import Smiley from "@Assets/Pic/Smiley.png";
 import { Input as InputBase } from "antd";
 import { Controller } from "react-hook-form";
 import classNames from "classnames";
+import moment from "moment";
 
 function AskAndAnswer() {
   const [chosenEmoji, setChosenEmoji] = useState(null);
@@ -16,10 +17,12 @@ function AskAndAnswer() {
   const [answer, setAnswer] = useState([
     {
       id: 1,
+      date: new Date(),
       txt: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.",
     },
   ]);
   const [input, setInput] = useState("");
+  const [date, setDate] = useState(new Date());
 
   const {
     handleSubmit,
@@ -27,7 +30,7 @@ function AskAndAnswer() {
     formState: { errors },
   } = formBox();
   const onSubmit = (e) => {
-    setData([...data, input]);
+    setData([...data, { txt: input, date: date }]);
     setInput("");
   };
   console.log(`object`, data);
@@ -55,17 +58,42 @@ function AskAndAnswer() {
               <div>
                 <div className="AskAndAnswer__askBox flex-col items-start">
                   <span>شما </span>
-                  <p>{index}</p>
+                  <p>{index.txt}</p>
                 </div>
+                {arr !== 0 && data[arr].date === data[arr - 1].date && (
+                  <div className="flex justify-end items-center w-4/5 AskAndAnswer__TimeBox-ask AskAndAnswer__TimeBox">
+                    <p>{moment(index.date).format("YYYY/MM/DD")}</p>
+                    <p>{moment(index.date).format("h:mm")}</p>
+                  </div>
+                )}
+                {arr === 0 && data.length === 1 && (
+                  <div className="flex justify-end items-center w-4/5 AskAndAnswer__TimeBox-ask AskAndAnswer__TimeBox">
+                    <p>{moment(index.date).format("YYYY/MM/DD")}</p>
+                    <p>{moment(index.date).format("h:mm")}</p>
+                  </div>
+                )}
                 {answer.map(
-                  (item) =>
+                  (item, idd) =>
                     item.id === arr && (
                       <div className="AskAndAnswer__answareBox ">
-                        <div className="AskAndAnswer__answare flex-col items-end">
+                        <div className="AskAndAnswer__answare flex-col items-start">
                           {" "}
                           <span>استاد </span>
                           <p> {item.txt}</p>
                         </div>
+                        {idd !== 0 &&
+                          answer[idd].date === answer[idd - 1].date && (
+                            <div className="flex justify-end items-center w-4/5 AskAndAnswer__TimeBox-answer AskAndAnswer__TimeBox">
+                              <p>{moment(index.date).format("h:mm")}</p>
+                              <p>{moment(index.date).format("YYYY/MM/DD")}</p>
+                            </div>
+                          )}
+                        {idd === 0 && answer.length === 1 && (
+                          <div className="flex justify-end items-center w-4/5 AskAndAnswer__TimeBox-answer AskAndAnswer__TimeBox">
+                            <p>{moment(index.date).format("h:mm")}</p>
+                            <p>{moment(index.date).format("YYYY/MM/DD")}</p>
+                          </div>
+                        )}
                       </div>
                     )
                 )}
