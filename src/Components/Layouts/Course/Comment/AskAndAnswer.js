3cafc885b9173b8c071043c66fd980d44436ed8e
Saba Beigi <beigisaba@gmail.com>
2021-10-28 00:53:27 +0330

    feat ask and aswer

--- src/Components/Layouts/Course/Comment/AskAndAnswer.js
+++ src/Components/Layouts/Course/Comment/AskAndAnswer.js
@@ -12,7 +12,13 @@ import classNames from "classnames";
 function AskAndAnswer() {
   const [chosenEmoji, setChosenEmoji] = useState(null);
   const [show, setShow] = useState(false);
-  const [data, setData] = useState();
+  const [data, setData] = useState([]);
+  const [answer, setAnswer] = useState([
+    {
+      id: 1,
+      txt: "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.",
+    },
+  ]);
   const [input, setInput] = useState("");
 
   const {
@@ -20,14 +26,12 @@ function AskAndAnswer() {
     control,
     formState: { errors },
   } = formBox();
-  const onSubmit = (data) => {
-      console.log(`object`, data)
+  const onSubmit = (e) => {
+    setData([...data, input]);
+    setInput("");
   };
+  console.log(`object`, data);
 
-  //   const onEmojiClick = (event, emojiObject) => {
-  //     setChosenEmoji(emojiObject);
-  //     setData(chosenEmoji data + );
-  //   };
   const addEmoji = (e) => {
     let sym = e.unified.split("-");
     let codesArray = [];
@@ -35,9 +39,41 @@ function AskAndAnswer() {
     let emoji = String.fromCodePoint(...codesArray);
     setInput(input + emoji);
   };
-  console.log(`input`, input);
+
   return (
     <div className="AskAndAnswer relative">
+      <div className="AskAndAnswer__content">
+        {data.length === 0 ? (
+          <div className="items-center absolute AskAndAnswer__emptyBox ">
+            <p className=" ">
+              سوالاتتان را در این بخش با استاد این دوره مطرح کنید.
+            </p>
+          </div>
+        ) : (
+          <div className="AskAndAnswer__contentBox">
+            {data.map((index, arr) => (
+              <div>
+                <div className="AskAndAnswer__askBox flex-col items-start">
+                  <span>شما </span>
+                  <p>{index}</p>
+                </div>
+                {answer.map(
+                  (item) =>
+                    item.id === arr && (
+                      <div className="AskAndAnswer__answareBox ">
+                        <div className="AskAndAnswer__answare flex-col items-end">
+                          {" "}
+                          <span>استاد </span>
+                          <p> {item.txt}</p>
+                        </div>
+                      </div>
+                    )
+                )}
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="AskAndAnswer__form absolute"
@@ -48,32 +84,27 @@ function AskAndAnswer() {
             onChange={(e) => setInput(e.target.value)}
             type="text"
             name="txt"
+            id="input"
             placeholder="پیام خود را بنویسید..."
             className={classNames("input__field", "AskAndAnswer__input")}
-            prefix={<Arroe />}
+            prefix={
+              <Arroe
+                onClick={handleSubmit(onSubmit)}
+                className="cursor-pointer	"
+              />
+            }
           />
         </div>
       </form>
       <div className="absolute AskAndAnswer__emogibox">
-        {chosenEmoji ? (
-          <span onClick={() => setShow(!show)}>{chosenEmoji.emoji}</span>
-        ) : (
-          <img
-            src={Smiley}
-            alt={Smiley}
-            className="cursor-pointer"
-            onClick={() => setShow(!show)}
-          />
-        )}
+        <img
+          src={Smiley}
+          alt={Smiley}
+          className="cursor-pointer"
+          onClick={() => setShow(!show)}
+        />
       </div>
-      {show && (
-        // <Picker
-        //   onEmojiClick={onEmojiClick}
-        //   preload={false}
-        //   className="AskAndAnswer__picker absolute"
-        // />
-        <Picker onSelect={addEmoji} />
-      )}
+      {show && <Picker onSelect={addEmoji} />}
     </div>
   );
 }
