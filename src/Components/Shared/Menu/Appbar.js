db4c453ab5e3d077483a33d31d3707687d28ebf9
Masoud Mir hosseini <sm2.masoud@gmail.com>
2021-10-28 12:30:09 +0330

    resp(Appbar):responsive appbar

--- src/Components/Shared/Menu/Appbar.js
+++ src/Components/Shared/Menu/Appbar.js
@@ -10,6 +10,7 @@ import UseScrollAppbar from './UseScrollAppbar';
 import logo from '@Assets/Logo/logo.svg';
 import { ReactComponent as ShoppingCartIcon } from '@Assets/Icons/shopping-cart.svg';
 import { ReactComponent as LogoTextIcon } from '@Assets/Logo/codeamooz-text.svg';
+import profile from '@Assets/Pic/profile.png';
 
 const menuItem = [
 	{
@@ -43,10 +44,19 @@ const menuItem = [
 		id: 6,
 	},
 ];
+
+//  is delete
+const isLogin = true;
+
 const Appbar = () => {
+	const [isOpenMenu, setOpenMenu] = useState(false);
 	const { pathname } = useLocation();
 	const dark = pathname === '/';
-	const {  sticky } = UseScrollAppbar();
+	const { sticky } = UseScrollAppbar();
+
+	const handleToggleMenu = () => {
+		setOpenMenu((prev) => !prev);
+	};
 
 	return (
 		<div
@@ -59,6 +69,7 @@ const Appbar = () => {
 					<div
 						className={classNames('menu_logo d-flex-align ', {
 							dark: dark && !sticky,
+							activeMenu: isOpenMenu,
 						})}
 					>
 						<div className='logo'>
@@ -69,20 +80,48 @@ const Appbar = () => {
 							<LogoTextIcon />
 						</div>
 					</div>
-					<nav className='Menu__nav d-flex-space'>
-						<ul className='Menu__ul d-flex-space list'>
-							{menuItem.map((item) => (
-								<li key={item.id} className='Menu__li'>
-									<NavLink exact to={item.url}>
-										{item.text}
-									</NavLink>
-								</li>
-							))}
-						</ul>
-					</nav>
-					<div className='Menu_actions'>
-						<IconBtn icon={<ShoppingCartIcon />} />
-						<RegisterBox /> 
+					<div className='Menu__nav--wrapper'>
+						<IconBtn
+							classes={classNames('Menu__nav--btn', {
+								open: isOpenMenu,
+								dark: dark && !sticky,
+							})}
+							icon={
+								<>
+									<span></span>
+									<span></span>
+									<span></span>
+								</>
+							}
+							onClick={handleToggleMenu}
+						/>
+						{isLogin && (
+							<div className='d-flex-align Menu__nav--profile'>
+								<p className='profile__name'>alireza_mzf</p>
+								<div className='profile__image'>
+									<img src={profile} alt='profile' />
+								</div>
+							</div>
+						)}
+						<nav
+							className={classNames('Menu__nav d-flex-space', {
+								active: isOpenMenu,
+							})}
+						>
+							<ul className='Menu__ul  list'>
+								{menuItem.map((item) => (
+									<li key={item.id} className='Menu__li'>
+										<NavLink exact to={item.url}>
+											{item.text}
+										</NavLink>
+									</li>
+								))}
+							</ul>
+							<div className='Menu_actions'>
+								<IconBtn icon={<ShoppingCartIcon />} />
+								<RegisterBox />
+							</div>
+						</nav>
 					</div>
 				</div>
 			</div>
--- src/Components/Shared/Menu/RegisterBox.js
+++ src/Components/Shared/Menu/RegisterBox.js
@@ -3,13 +3,13 @@ import React from 'react';
 import profile from '@Assets/Pic/profile.png';
 import Link from '../Buttons/Link';
 //  is delete
-const isLogin = false;
+const isLogin = true;
 
 const RegisterBox = () => {
 	return (
 		<>
 			{isLogin ? (
-				<div className='d-flex-align '>
+				<div className='d-flex-align Menu_actions--profile'>
 					<p className='profile__name'>alireza_mzf</p>
 					<div className='profile__image'>
 						<img src={profile} alt='profile' />
