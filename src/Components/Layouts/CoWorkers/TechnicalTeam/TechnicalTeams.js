e1d7cf3378e017206282d79705df257424567e59
Masoud Mir hosseini <sm2.masoud@gmail.com>
2021-10-30 11:17:05 +0330

    feat(Tabs):set Tabs component in project

--- src/Components/Layouts/CoWorkers/TechnicalTeam/TechnicalTeams.js
+++ src/Components/Layouts/CoWorkers/TechnicalTeam/TechnicalTeams.js
@@ -1,58 +1,58 @@
-import React, { useState, useEffect } from "react";
-import { useParams, useLocation } from "react-router-dom";
-import BreadCrump from "@Components/Shared/BreadCrump/BreadCrump";
-import Deatil from "./Deatil";
-import Request from "./Request";
-import TabBox from "@Components/Shared/Tabs/TabBox";
-import { Tabs } from "antd";
+import React, { useState, useEffect } from 'react';
+import { useParams, useLocation } from 'react-router-dom';
+import BreadCrump from '@Components/Shared/BreadCrump/BreadCrump';
+import Deatil from './Deatil';
+import Request from './Request';
+import TabBox from '@Components/Shared/Tabs/TabBox';
+import { Tabs } from 'antd';
 
 const { TabPane } = Tabs;
 
 function TechnicalTeams() {
-  const { name } = useParams();
-  const location = useLocation();
-  const [titles, setTitles] = useState("");
-  const [menu, setMenu] = useState([]);
-  const [id, setId] = useState(0);
+	const { name } = useParams();
+	const location = useLocation();
+	const [titles, setTitles] = useState('');
+	const [menu, setMenu] = useState([]);
+	const [id, setId] = useState(0);
 
-  useEffect(() => {
-    setTitles(location.state.title);
-    setMenu([
-      {
-        name: "صفحه اصلی",
-        rout: "",
-      },
-      {
-        name: "همکاران و اساتید",
-        rout: "coWorkers",
-      },
-      {
-        name: "technical Team",
-        rout: "coWorkers/technicalteam",
-      },
-      {
-        name: location.state.title,
-        rout: `coWorkers/technicalteam/${name}`,
-      },
-    ]);
-  }, [location]);
+	useEffect(() => {
+		setTitles(location.state.title);
+		setMenu([
+			{
+				name: 'صفحه اصلی',
+				rout: '',
+			},
+			{
+				name: 'همکاران و اساتید',
+				rout: 'coWorkers',
+			},
+			{
+				name: 'technical Team',
+				rout: 'coWorkers/technicalteam',
+			},
+			{
+				name: location.state.title,
+				rout: `coWorkers/technicalteam/${name}`,
+			},
+		]);
+	}, [location]);
 
-  return (
-    <div className="TechnicalTeams">
-      {menu.length !== 0 && <BreadCrump item={menu} />}
-      <p className="Master__title text-center">{titles}</p>
-      <div className="Master__formBox">
-        <TabBox className="Master__form" key="1">
-          <TabPane tab="جزئیات" key="1">
-            <Deatil />
-          </TabPane>
-          <TabPane tab="ارسال درخواست" key="2">
-            <Request />
-          </TabPane>
-        </TabBox>
-      </div>
-    </div>
-  );
+	return (
+		<div className='TechnicalTeams'>
+			{menu.length !== 0 && <BreadCrump item={menu} />}
+			<p className='Master__title text-center'>{titles}</p>
+			<div className='Master__formBox'>
+				<Tabs className='TabBox' type='card'>
+					<TabPane tab='جزئیات' key='1'>
+						<Deatil />
+					</TabPane>
+					<TabPane tab='ارسال درخواست' key='2'>
+						<Request />
+					</TabPane>
+				</Tabs>
+			</div>
+		</div>
+	);
 }
 
 export default TechnicalTeams;
--- src/Layouts/Course/index.js
+++ src/Layouts/Course/index.js
@@ -1,66 +1,66 @@
-import React, { useState, useEffect } from "react";
-import { useHistory, useLocation } from "react-router-dom";
-import BreadCrump from "@Components/Shared/BreadCrump/BreadCrump";
-import SideBarDiscount from "@Components/Shared/Discount/SideBarDiscount";
-import HeaderDiscount from "@Components/Shared/Discount/HeaderDiscount";
-import Sarfasl from "@Components/Layouts/Course/Sarfasl";
-import About from "@Components/Layouts/Course/About";
-import Comment from "@Components/Layouts/Course/Comment/Comment";
-import AskAndAnswer from "@Components/Layouts/Course/Comment/AskAndAnswer";
-import TabBox from "@Components/Shared/Tabs/TabBox";
-import { Tabs } from "antd";
+import React, { useState, useEffect } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import BreadCrump from '@Components/Shared/BreadCrump/BreadCrump';
+import SideBarDiscount from '@Components/Shared/Discount/SideBarDiscount';
+import HeaderDiscount from '@Components/Shared/Discount/HeaderDiscount';
+import Sarfasl from '@Components/Layouts/Course/Sarfasl';
+import About from '@Components/Layouts/Course/About';
+import Comment from '@Components/Layouts/Course/Comment/Comment';
+import AskAndAnswer from '@Components/Layouts/Course/Comment/AskAndAnswer';
+import TabBox from '@Components/Shared/Tabs/TabBox';
+import { Tabs } from 'antd';
 
 const { TabPane } = Tabs;
 
 function Index() {
-  const location = useLocation();
-  useEffect(() => {
-    setMenu([
-      {
-        name: "صفحه اصلی",
-        rout: "",
-      },
-      {
-        name: "دوره ها",
-        rout: "/courses",
-      },
-      {
-        name: location.state.name,
-        rout: "/course",
-      },
-    ]);
-  }, [location]);
-  const [menu, setMenu] = useState([]);
-  return (
-    <div className="container">
-      <BreadCrump item={menu} />
-      <div className="Course">
-        <HeaderDiscount />
-        <div className="grid Course__container">
-          <div></div>
-          <div>
-            <TabBox className="Sarfasl__form" key="1">
-              <TabPane tab="درباره این دوره" key="1">
-                <About />
-              </TabPane>
-              <TabPane tab="سرفصل ها" key="2">
-                <Sarfasl />
-              </TabPane>
-            </TabBox>
-            <TabBox className="Sarfasl__commentpart" key="2">
-              <TabPane tab="نظرات" key="1">
-                <Comment />
-              </TabPane>
-              <TabPane tab="پرسش و پاسخ" key="2">
-                <AskAndAnswer />
-              </TabPane>
-            </TabBox>
-          </div>
-          <div className="fixed">{/* <SideBarDiscount /> */}</div>
-        </div>
-      </div>
-    </div>
-  );
+	const location = useLocation();
+	useEffect(() => {
+		setMenu([
+			{
+				name: 'صفحه اصلی',
+				rout: '',
+			},
+			{
+				name: 'دوره ها',
+				rout: '/courses',
+			},
+			{
+				name: location.state.name,
+				rout: '/course',
+			},
+		]);
+	}, [location]);
+	const [menu, setMenu] = useState([]);
+	return (
+		<div className='container'>
+			<BreadCrump item={menu} />
+			<div className='Course'>
+				<HeaderDiscount />
+				<div className='grid Course__container'>
+					<div></div>
+					<div>
+						<Tabs className='TabBox' type='card'>
+							<TabPane tab='درباره این دوره' key='1'>
+								<About />
+							</TabPane>
+							<TabPane tab='سرفصل ها' key='2'>
+								<Sarfasl />
+							</TabPane>
+						</Tabs>
+						<Tabs className='TabBox' type='card'>
+							<TabPane tab='نظرات' key='1'>
+								<Comment />
+							</TabPane>
+							<TabPane tab='پرسش و پاسخ' key='2' >
+								<AskAndAnswer />
+							</TabPane>
+						</Tabs>
+					</div>
+					<div className='fixed'>{/* <SideBarDiscount /> */}</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Index;
