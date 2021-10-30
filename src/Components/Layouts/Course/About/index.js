c76b3a13491a6d47b786719afba7d7452acde4ec
Saba Beigi <beigisaba@gmail.com>
2021-10-30 04:54:22 +0330

    feat about in course

--- src/Components/Layouts/Course/About/index.js
+++ src/Components/Layouts/Course/About/index.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import Detaile from "./Detaile";
+import VideoPlayer from "@Components/Shared/VideoPlayer/VideoPlayer";
+
 
 function index() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <Detaile />
+      <VideoPlayer src="" />
+    </div>
+  );
 }
 
-export default index
+export default index;
--- src/Layouts/Course/index.js
+++ src/Layouts/Course/index.js
@@ -64,4 +64,3 @@ function Index() {
 }
 
 export default Index;
-
