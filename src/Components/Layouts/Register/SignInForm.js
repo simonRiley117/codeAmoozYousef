5efb290df74ebe387457467aaab7d64a12b02663
hoesseinmh <akhoundi22@gmail.com>
2021-11-08 12:45:32 +0330

    add new integration for signin and signup components

--- src/Components/Layouts/Register/SignInForm.js
+++ src/Components/Layouts/Register/SignInForm.js
@@ -1,53 +1,81 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
-import Input from '@Components/Shared/Inputs/Input';
-import Password from '@Components/Shared/Inputs/Password';
+import Input from "@Components/Shared/Inputs/Input";
+import Password from "@Components/Shared/Inputs/Password";
 
-import { ReactComponent as UserIcon } from '@Assets/Icons/user.svg';
-import { ReactComponent as LockIcon } from '@Assets/Icons/lock.svg';
-import { Checkbox } from 'antd';
-import Button from '@Components/Shared/Buttons/Button';
+import { ReactComponent as UserIcon } from "@Assets/Icons/user.svg";
+import { ReactComponent as LockIcon } from "@Assets/Icons/lock.svg";
+import { Checkbox } from "antd";
+// import useFetch from "../../../Contexts/useFetch";
+// import { useAuth } from "../../../Contexts/authContext";
+import Button from "@Components/Shared/Buttons/Button";
+import useFetch from "../../../Context/useFetch";
+import { useAuth } from "@App/Context/authContext";
 
 const SignInForm = () => {
-	const {
-		handleSubmit,
-		control,
-		formState: { errors },
-	} = useForm();
+  const { authDispatch } = useAuth();
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
+  const [postData, setPostData] = useState();
 
-	const onSubmit = (data) => {};
+  const loginRequest = useFetch({
+    url: `auth/login`,
+    method: "POST",
+    data: postData,
+    trigger: false,
+    noHeader: true,
+    argFunc: (res) => {
+      // setLoading(false);
 
-	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<div className='register__form--inner'>
-				<Input
-					label='نام کاربری'
-					register={{
-						required: true,
-					}}
-					message='نام کاربری را وارد کنید'
-					name='userName'
-					control={control}
-					errors={errors}
-					prefix={<UserIcon />}
-				/>
-				<Password
-					label='رمز عبور'
-					register={{
-						required: true,
-					}}
-					message='رمز عبور را وارد کنید'
-					name='password'
-					control={control}
-					errors={errors}
-					prefix={<LockIcon />}
-				/>
-				<Checkbox>منو به خاطر بسپار</Checkbox>
-				<Button htmlType='submit'>ورود</Button>
-				
-			</div>
-		</form>
-	);
+      authDispatch({
+        type: "LOGIN",
+        token: res.access_token,
+        refresh: res.refresh_token,
+      });
+      // history.push("/panel");
+    },
+  });
+
+  const onSubmit = (data) => {
+    setPostData({ username: data.username, password: data.password });
+    loginRequest.reFetch();
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="register__form--inner">
+        <Input
+          label="نام کاربری"
+          register={{
+            required: true,
+          }}
+          message="نام کاربری را وارد کنید"
+          name="username"
+          control={control}
+          errors={errors}
+          prefix={<UserIcon />}
+        />
+        <Password
+          label="رمز عبور"
+          register={{
+            required: true,
+          }}
+          message="رمز عبور را وارد کنید"
+          name="password"
+          control={control}
+          errors={errors}
+          prefix={<LockIcon />}
+        />
+        <Checkbox>منو به خاطر بسپار</Checkbox>
+        <Button disabled={loginRequest.loading} htmlType="submit">
+          ورود
+        </Button>
+      </div>
+    </form>
+  );
 };
 export default SignInForm;
--- src/Components/Layouts/Register/SignUpForm.js
+++ src/Components/Layouts/Register/SignUpForm.js
@@ -1,76 +1,150 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import Input from '@Components/Shared/Inputs/Input';
-import Password from '@Components/Shared/Inputs/Password';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import Input from "@Components/Shared/Inputs/Input";
+import Password from "@Components/Shared/Inputs/Password";
 
-import { ReactComponent as UserIcon } from '@Assets/Icons/user.svg';
-import { ReactComponent as LockIcon } from '@Assets/Icons/lock.svg';
-import { Checkbox } from 'antd';
-import Button from '@Components/Shared/Buttons/Button';
-import { Link } from 'react-router-dom';
+import { ReactComponent as UserIcon } from "@Assets/Icons/user.svg";
+import { ReactComponent as LockIcon } from "@Assets/Icons/lock.svg";
+import { Checkbox } from "antd";
+import Button from "@Components/Shared/Buttons/Button";
+import { Link } from "react-router-dom";
+import useFetch from "../../../Context/useFetch";
 
-const SignUpForm = () => {
-	const {
-		handleSubmit,
-		control,
-		formState: { errors },
-	} = useForm();
+const SignUpForm = ({ handleActive }) => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+    reset,
+  } = useForm();
 
-	const onSubmit = (data) => {};
+  const [postData, setPostData] = useState();
+  const [checkBoxState, setCheckBoxState] = useState(false);
 
-	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<div className='register__form--inner signUp'>
-				<Input
-					label='نام کاربری'
-					register={{
-						required: true,
-					}}
-					message='نام کاربری را وارد کنید'
-					name='userName'
-					control={control}
-					errors={errors}
-					prefix={<UserIcon />}
-				/>
-				<Input
-					label='ایمیل'
-					register={{
-						required: true,
-					}}
-					message='ایمیل را وارد کنید'
-					name='userName'
-					control={control}
-					errors={errors}
-					prefix={<UserIcon />}
-				/>
-				<Password
-					label='رمز عبور'
-					register={{
-						required: true,
-					}}
-					message='رمز عبور را وارد کنید'
-					name='password'
-					control={control}
-					errors={errors}
-					prefix={<LockIcon />}
-				/>
-				<Password
-					label='تکرار رمز عبور'
-					register={{
-						required: true,
-					}}
-					message='رمز عبور را وارد کنید'
-					name='password'
-					control={control}
-					errors={errors}
-					prefix={<LockIcon />}
-				/>
-				<Checkbox>
-					<Link>قوانین و مقررات</Link> سایت را قبول دارم
-				</Checkbox>
-				<Button htmlType='submit'>ثبت نام</Button>
-			</div>
-		</form>
-	);
+  const registerRequest = useFetch({
+    url: `auth/register`,
+    method: "POST",
+    data: postData,
+    trigger: false,
+    noHeader: true,
+    errMessage: (mess) => {
+      let showMessage = "";
+      if ("password1" in mess)
+        if (
+          mess.password1.includes(
+            "This password is too short. It must contain at least 8 characters."
+          ) ||
+          mess.password1.includes("This password is too common.") ||
+          mess.password1.includes("This password is entirely numeric.")
+        ) {
+          showMessage = "pass";
+        }
+      if ("username" in mess)
+        if (
+          mess.username.includes("A user with that username already exists.")
+        ) {
+          showMessage = "user";
+        }
+      if ("email" in mess)
+        if (
+          mess.email.includes(
+            "A user is already registered with this e-mail address."
+          ) ||
+          mess.email.includes("Enter a valid email address.")
+        ) {
+          showMessage = "email";
+        }
+      //   toast.error(showMessage, {
+      //     position: toast.POSITION.TOP_CENTER,
+      //   });
+    },
+
+    argFunc: (res) => {
+      if (res.detail == "Verification e-mail sent.") {
+        handleActive();
+        // reset();
+        //   toast.error(showMessage, {
+        //     position: toast.POSITION.TOP_CENTER,
+        //   });
+      }
+    },
+  });
+
+  const onSubmit = (data) => {
+    if (data.password1 == data.password2) {
+      setPostData({
+        username: data.username,
+        password1: data.password1,
+        password2: data.password2,
+        email: data.email,
+      });
+      registerRequest.reFetch();
+    } else {
+      //! send error message
+    }
+  };
+
+  function onChange(e) {
+    setCheckBoxState(e.target.checked);
+  }
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="register__form--inner signUp">
+        <Input
+          label="نام کاربری"
+          register={{
+            required: true,
+          }}
+          message="نام کاربری را وارد کنید"
+          name="username"
+          control={control}
+          errors={errors}
+          prefix={<UserIcon />}
+        />
+        <Input
+          label="ایمیل"
+          register={{
+            required: true,
+          }}
+          message="ایمیل را وارد کنید"
+          name="email"
+          control={control}
+          errors={errors}
+          prefix={<UserIcon />}
+        />
+        <Password
+          label="رمز عبور"
+          register={{
+            required: true,
+          }}
+          message="رمز عبور را وارد کنید"
+          name="password1"
+          control={control}
+          errors={errors}
+          prefix={<LockIcon />}
+        />
+        <Password
+          label="تکرار رمز عبور"
+          register={{
+            required: true,
+          }}
+          message="رمز عبور را وارد کنید"
+          name="password2"
+          control={control}
+          errors={errors}
+          prefix={<LockIcon />}
+        />
+        <Checkbox checked={checkBoxState} onChange={onChange}>
+          <Link>قوانین و مقررات</Link> سایت را قبول دارم
+        </Checkbox>
+        <Button
+          disabled={registerRequest.loading || !checkBoxState ? true : false}
+          htmlType="submit"
+        >
+          ثبت نام
+        </Button>
+      </div>
+    </form>
+  );
 };
 export default SignUpForm;
