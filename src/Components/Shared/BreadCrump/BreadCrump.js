cbf0654ea640041c71d6ad3179be3edc8350caa8
parisa <parisaghorbany77@gmail.com>
2021-11-22 11:24:28 +0330

    fix(breadcrumps)

--- src/Components/Shared/BreadCrump/BreadCrump.js
+++ src/Components/Shared/BreadCrump/BreadCrump.js
@@ -6,37 +6,6 @@ import { Breadcrumb as BreadcrumbBase } from "antd";
 import { ReactComponent as SeparatorIcon } from "@Assets/Icons/SeparatorIcon.svg";
 const { Item } = BreadcrumbBase;
 
-<<<<<<< HEAD
-const BreadCrump = ({ title }) => {
-	const routes = [
-		{ path: '/', breadcrumb: 'صفحه اصلی' },
-		{ path: '/news', breadcrumb: 'اخبار' },
-		{ path: '/news/news-info', breadcrumb: title },
-		{ path: '/about-me', breadcrumb: 'درباره ما' },
-		{ path: '/mycourse', breadcrumb: 'دوره های من' },
-		{ path: '/wallet', breadcrumb: 'کیف پول' },
-		{ path: '/setting', breadcrumb: 'تنظیمات' },
-		{ path: '/sabadkala', breadcrumb: 'سبدخرید' },
-		{ path: '/mycourse/createcourse', breadcrumb: 'ایجاد دوره' },
-		{ path: '/mycourse/createseasons', breadcrumb: 'جلسات دوره ' },
-		{
-			path: '/mycourse/createseasons/createcontent',
-			breadcrumb: ' محتوای مبحث ',
-		},
-	];
-	const breadcrumbs = useBreadcrumbs(routes);
-	return (
-		<div className='breadcrumbs'>
-			<BreadcrumbBase className='breadcrumb' separator={<SeparatorIcon />}>
-				{breadcrumbs.map(({ match, breadcrumb }) => (
-					<Item key={match.url}>
-						<NavLink to={match.url}>{breadcrumb}</NavLink>
-					</Item>
-				))}
-			</BreadcrumbBase>
-		</div>
-	);
-=======
 const BreadCrump = ({ title, pathsname, name }) => {
   const routes = [
     { path: "/", breadcrumb: "صفحه اصلی" },
@@ -49,6 +18,7 @@ const BreadCrump = ({ title, pathsname, name }) => {
     { path: "/mycourse/createcourse", breadcrumb: "ایجاد دوره" },
     { path: "/mycourse/createseasons", breadcrumb: "جلسات دوره " },
     { path: "/dash", breadcrumb: "داشبورد" },
+    { path: '/sabadkala', breadcrumb: 'سبدخرید' },
     { path: pathsname, breadcrumb: name },
     {
       path: "/mycourse/createseasons/createcontent",
@@ -67,7 +37,6 @@ const BreadCrump = ({ title, pathsname, name }) => {
       </BreadcrumbBase>
     </div>
   );
->>>>>>> e0916998f392676be2453f71b9b80538d61129a6
 };
 
 BreadCrump.defaultProps = {
