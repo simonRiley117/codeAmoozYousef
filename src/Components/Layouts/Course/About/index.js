6ecaefa6f0df27b716697974c21d4bef1f902a40
Saba Beigi <beigisaba@gmail.com>
2021-12-08 01:54:36 +0330

    change id

--- src/Components/Layouts/Course/About/index.js
+++ src/Components/Layouts/Course/About/index.js
@@ -1,60 +1,64 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Detaile from "./Detaile";
 import VideoPlayer from "@Components/Shared/VideoPlayer/VideoPlayer";
 import useFetch from "@App/Context/useFetch";
 
+function Index({ courseId }) {
+  const [courseInfo, setCourseInfo] = useState({});
 
-function Index({courseId}) {
-    const [courseInfo, setCourseInfo] = useState({})
+  const getAboutCourse = useFetch({
+    url: `CourseService/${courseId}`,
+    // url: `CourseService/${id}/courseOrder`,
+    method: "GET",
+    noHeader: true,
+    setter: setCourseInfo,
+  });
 
-    const getAboutCourse = useFetch({
-        url: `CourseService/react`,
-        // url: `CourseService/${id}/courseOrder`,
-        method: "GET",
-        noHeader: true,
-        setter: setCourseInfo
-    });
+  // console.log('getAboutCourse.loading: ', getAboutCourse.loading)
+  // console.log('getAboutCourse: ', getAboutCourse)
+  const {
+    title,
+    intro,
+    cover,
+    // language,
+    intro_video,
+    // percentage_of_quality_of_course,
+    properties,
+    prerequisites,
+  } = courseInfo;
 
-    // console.log('getAboutCourse.loading: ', getAboutCourse.loading)
-    // console.log('getAboutCourse: ', getAboutCourse)
-    const {
-        title,
-        intro,
-        cover,
-        // language,
-        intro_video,
-        // percentage_of_quality_of_course,
-        properties,
-        prerequisites
-    } = courseInfo
-
-    return (
-        <>
-            {getAboutCourse?.response
-                ?<div>
-                    <Detaile title={title} intro={intro} cover={cover}/>
-                    <VideoPlayer src={intro_video}/>
-                    <div className="AboutDetaile__hederBox">
-                        <p className="font-bold">ویژگی ها</p>
-                    </div>
-                    {properties.map(property => (
-                        <p key={property.id} className="AboutDetaile__txt leading-loose About__list">
-                            {property.prop}
-                        </p>
-                    ))}
-                    <div className="AboutDetaile__hederBox About__header">
-                        <p className="font-bold">پیش نیاز ها</p>
-                    </div>
-                    {prerequisites.map(prerequisite => (
-                        <p key={prerequisite.id} className="AboutDetaile__txt leading-loose About__list">
-                            {prerequisite.pre_prop}
-                        </p>
-                    ))}
-                </div>
-                :null
-            }
-        </>
-    );
+  return (
+    <>
+      {getAboutCourse?.response ? (
+        <div>
+          <Detaile title={title} intro={intro} cover={cover} />
+          <VideoPlayer src={intro_video} />
+          <div className="AboutDetaile__hederBox">
+            <p className="font-bold">ویژگی ها</p>
+          </div>
+          {properties.map((property) => (
+            <p
+              key={property.id}
+              className="AboutDetaile__txt leading-loose About__list"
+            >
+              {property.prop}
+            </p>
+          ))}
+          <div className="AboutDetaile__hederBox About__header">
+            <p className="font-bold">پیش نیاز ها</p>
+          </div>
+          {prerequisites.map((prerequisite) => (
+            <p
+              key={prerequisite.id}
+              className="AboutDetaile__txt leading-loose About__list"
+            >
+              {prerequisite.pre_prop}
+            </p>
+          ))}
+        </div>
+      ) : null}
+    </>
+  );
 }
 
 export default Index;
