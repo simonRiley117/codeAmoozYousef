bd92f0b07095475a633f6c198fff9ef05f3d85a4
Masoud Mir hosseini <sm2.masoud@gmail.com>
2021-12-21 18:55:48 +0330

    fix(CourseContent):fixed accordian

--- src/Components/Layouts/CourseContent/SeasonList.js
+++ src/Components/Layouts/CourseContent/SeasonList.js
@@ -1,49 +1,49 @@
-import React, { useEffect, useState } from "react";
-import { Accordion } from "@Components/Shared/Accordion/Accordion";
-import SeasonsItem from "./SeasonsItem";
-import { useLocation } from "react-router";
-import useFetch from "../../../Context/useFetch";
+import React, { useEffect, useState } from 'react';
+import { Accordion } from '@Components/Shared/Accordion/Accordion';
+import SeasonsItem from './SeasonsItem';
+import { useLocation } from 'react-router';
+import useFetch from '../../../Context/useFetch';
 
 const SeasonList = ({
-  activeContent,
-  sidebarList,
-  activeSeasons,
-  setquizUuid,
-  getContentName,
-  changeContentID,
-  setIsContentPass
+	activeContent,
+	sidebarList,
+	activeSeasons,
+	setquizUuid,
+	getContentName,
+	changeContentID,
+	setIsContentPass,
 }) => {
-  let [openPanels, setOpenPanels] = React.useState([activeSeasons]);
-  useEffect(() => {
-    if(!openPanels.includes(activeSeasons)){
-      openPanels.push(activeSeasons)
-    }
-  }, [,activeSeasons])
-  return (
-    <div className="Sarfasl__Accordionbox">
-      <Accordion
-        defaultActiveKey={activeSeasons}
-        activeKey={openPanels}
-        onChange={setOpenPanels}
-        destroyInactivePanel={true}
-      >
-        {sidebarList.seasons.map((season, index) => (
-          <SeasonsItem
-            setquizUuid={setquizUuid}
-            changeContentID={changeContentID}
-            activeContent={activeContent}
-            openPanels={openPanels}
-            activeSeasons={activeSeasons}
-            key={season.uuid}
-            season={season}
-            index={index}
-            getContentName={getContentName}
-            setIsContentPass={setIsContentPass}
-          />
-        ))}
-      </Accordion>
-    </div>
-  );
+	const [openPanels, setOpenPanels] = useState([activeSeasons]);
+	useEffect(() => {
+		if (!openPanels.includes(activeSeasons)) {
+			setOpenPanels((before) => [...before, activeSeasons]);
+		}
+	}, [activeSeasons]);
+	return (
+		<div className='Sarfasl__Accordionbox'>
+			<Accordion
+				defaultActiveKey={activeSeasons}
+				// activeKey={openPanels}
+				// onChange={setOpenPanels}
+				destroyInactivePanel
+			>
+				{sidebarList.seasons.map((season, index) => (
+					<SeasonsItem
+						setquizUuid={setquizUuid}
+						changeContentID={changeContentID}
+						activeContent={activeContent}
+						openPanels={openPanels}
+						activeSeasons={activeSeasons}
+						key={season.uuid}
+						season={season}
+						index={index}
+						getContentName={getContentName}
+						setIsContentPass={setIsContentPass}
+					/>
+				))}
+			</Accordion>
+		</div>
+	);
 };
 
 export default SeasonList;
