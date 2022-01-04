79c2754c2f8d246274c2012c5121382155128522
Masoud Mir hosseini <sm2.masoud@gmail.com>
2022-01-04 17:14:57 +0330

    fix(Appbar):fixed scroll page

--- src/Components/Shared/Menu/Appbar.js
+++ src/Components/Shared/Menu/Appbar.js
@@ -1,51 +1,35 @@
 import React, { useState } from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useLocation } from 'react-router';
 import { useNavigate } from 'react-router-dom';
-import useFetch from '@App/Context/useFetch';
 
 import IconBtn from '@Components/Shared/Buttons/IconBtn';
 import classNames from 'classnames';
 import UseScrollAppbar from './UseScrollAppbar';
 import Button from '../Buttons/Button';
 import Register from '@Components/Layouts/Register/Register';
-import { toast } from 'react-toastify';
-import Cookies from 'js-cookie';
-import Modal from '@Components/Shared/Modal/Modal';
+
 
 // Assets
 import logo from '@Assets/Logo/logo.svg';
 import { ReactComponent as ShoppingCartIcon } from '@Assets/Icons/shopping-cart.svg';
 import { ReactComponent as LogoTextIcon } from '@Assets/Logo/codeamooz-text.svg';
-import { Divider, Popover } from 'antd';
-import {
-	DownOutlined,
-	LeftOutlined,
-	HomeOutlined,
-	HeartOutlined,
-	LogoutOutlined,
-	UserOutlined,
-} from '@ant-design/icons';
+
 import { useAuth } from '@App/Context/authContext';
 import { useUserData } from '@App/Context/userContext';
 import { useEffect } from 'react';
-import { ReactComponent as ExiteIcon } from '@Assets/Icons/quite.svg';
-import { ReactComponent as UserIcon } from '@Assets/Icons/user.svg';
-import { ReactComponent as Heart } from '@Assets/Icons/heart.svg';
+
 import Propfile from './ProfileMenu/Propfile';
 
 const Appbar = () => {
 	const navigate = useNavigate();
 	const [isOpenMenu, setOpenMenu] = useState(false);
 	const [isModalVisible, setModalVisible] = useState(false);
-	const [hoverMenu, setHoverMenu] = useState(false);
-	const [modal, setModal] = useState(false);
 	const { userData } = useUserData();
 	const { pathname, search } = useLocation();
 	const dark = pathname === '/';
 	const { sticky } = UseScrollAppbar();
 	const { token } = useAuth();
-	const { authDispatch } = useAuth();
 
 	const menuItem = [
 		{
@@ -93,10 +77,7 @@ const Appbar = () => {
 		setModalVisible((prev) => !prev);
 	};
 
-	const handleModalShow = (uuid, lock) => {
-		setModal((prev) => !prev);
-	};
-
+	
 	useEffect(() => {
 		if (dark && !token && search == '?redirectTeacher') {
 			handleModalVisible();
@@ -109,102 +90,105 @@ const Appbar = () => {
 		}
 	}, []);
 
-  return (
-    <>
-      <div
-        className={classNames("Menu__sec primary", {
-          sticky: sticky,
-        })}
-      >
-        <div className="container">
-          <div className="Menu-wrapper">
-            <div
-              className={classNames("menu_logo d-flex-align ", {
-                dark: dark && !sticky,
-                activeMenu: isOpenMenu,
-              })}
-            >
-              <div className="logo">
-                <img src={logo} alt="logo" />
-              </div>
-              {/* <img src={Codeamoozlogo} alt='codeamooz' /> */}
-              <div className="logo-text">
-                <LogoTextIcon />
-              </div>
-            </div>
-            <div className="Menu__nav--wrapper">
-              <IconBtn
-                classes={classNames("Menu__nav--btn", {
-                  open: isOpenMenu,
-                  dark: dark && !sticky,
-                })}
-                icon={
-                  <>
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                  </>
-                }
-                onClick={handleToggleMenu}
-              />
-              {token && (
-                <div className="d-flex-align Menu__nav--profile">
-                  <p className="profile__name">
-                    {userData !== null ? userData.username : ""}
-                  </p>
-                  <div className="profile__image">
-                    <img src={userData.cover} alt="profile" />
-                  </div>
-                </div>
-              )}
-              <nav
-                className={classNames("Menu__nav d-flex-space", {
-                  active: isOpenMenu,
-                })}
-              >
-                <ul className="Menu__ul  list">
-                  {menuItem.map((item) => (
-                    <li
-                      key={item.id}
-                      className="Menu__li"
-                      onClick={() => setOpenMenu(false)}
-                    >
-                      <NavLink to={item.url}>{item.text}</NavLink>
-                    </li>
-                  ))}
-                </ul>
-                <div className="Menu_actions">
-                  {token && (
-                    <div className=" relative">
-                      <IconBtn
-                        icon={<ShoppingCartIcon />}
-                        onClick={() => navigate("/shopping-card")}
-                      />
-                      <div className="Menu_action--badge ">
-                        <p>{userData.cart}</p>
-                      </div>
-                    </div>
-                  )}
-                  {token ? (
-                    <div className="d-flex-align Menu_actions--profile">
-                      <div className={classNames("profile__image")}>
-                        <Propfile />
-                      </div>
-                    </div>
-                  ) : (
-                    <Button type="primary" onClick={handleModalVisible}>
-                      ورود / ثبت نام
-                    </Button>
-                  )}
-                </div>
-              </nav>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Register visible={isModalVisible} onCancel={handleModalVisible} />
-    </>
-  );
+	return (
+		<>
+			<div
+				className={classNames('Menu__sec primary', {
+					sticky: sticky,
+				})}
+			>
+				<div className='container'>
+					<div className='Menu-wrapper'>
+						<div
+							className={classNames('menu_logo d-flex-align ', {
+								dark: dark && !sticky,
+								activeMenu: isOpenMenu,
+							})}
+						>
+							<div className='logo'>
+								<img src={logo} alt='logo' />
+							</div>
+							{/* <img src={Codeamoozlogo} alt='codeamooz' /> */}
+							<div className='logo-text'>
+								<LogoTextIcon />
+							</div>
+						</div>
+						<div className='Menu__nav--wrapper'>
+							<IconBtn
+								classes={classNames('Menu__nav--btn', {
+									open: isOpenMenu,
+									dark: dark && !sticky,
+								})}
+								icon={
+									<>
+										<span></span>
+										<span></span>
+										<span></span>
+									</>
+								}
+								onClick={handleToggleMenu}
+							/>
+							{token && (
+								<div className='d-flex-align Menu__nav--profile'>
+									<p className='profile__name'>
+										{userData !== null ? userData.username : ''}
+									</p>
+									<div className='profile__image'>
+										<img src={userData.cover} alt='profile' />
+									</div>
+								</div>
+							)}
+							<nav
+								className={classNames('Menu__nav d-flex-space', {
+									active: isOpenMenu,
+								})}
+							>
+								<ul className='Menu__ul  list'>
+									{menuItem.map((item) => (
+										<li
+											key={item.id}
+											className='Menu__li'
+											onClick={() => handleToggleMenu()}
+										>
+											<NavLink to={item.url}>{item.text}</NavLink>
+										</li>
+									))}
+								</ul>
+								<div className='Menu_actions'>
+									{token && (
+										<div className=' relative'>
+											<IconBtn
+												icon={<ShoppingCartIcon />}
+												onClick={() => navigate('/shopping-card')}
+											/>
+											<div className='Menu_action--badge '>
+												<p>{userData.cart}</p>
+											</div>
+										</div>
+									)}
+									{token ? (
+										<div className='d-flex-align Menu_actions--profile'>
+											<div className={classNames('profile__image')}>
+												<Propfile />
+											</div>
+										</div>
+									) : (
+										<Button
+											type='primary'
+											onClick={handleModalVisible}
+										>
+											ورود / ثبت نام
+										</Button>
+									)}
+								</div>
+							</nav>
+						</div>
+					</div>
+				</div>
+			</div>
+			<Register visible={isModalVisible} onCancel={handleModalVisible} />
+		</>
+	);
 };
 
 export default Appbar;
