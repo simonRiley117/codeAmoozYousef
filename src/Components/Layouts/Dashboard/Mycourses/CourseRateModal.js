df6529c16bd6713bcbaacec0e38d86f658110920
pt-ubuntu-os <persian1379pr@gmail.com>
2022-01-09 01:21:00 +0330

    feat(do score on finished course)

--- src/Components/Layouts/Dashboard/Mycourses/CourseRateModal.js
+++ src/Components/Layouts/Dashboard/Mycourses/CourseRateModal.js
@@ -1,35 +1,71 @@
-import React from "react";
-import { Rate } from "antd";
-import { ReactComponent as RateIcon } from "@Assets/Icons/Rate.svg";
+import React, {useState} from "react";
+import {Rate} from "antd";
+import {ReactComponent as RateIcon} from "@Assets/Icons/Rate.svg";
 import Textarea from "@Components/Shared/Inputs/Textarea";
-import { useForm as formBox } from "react-hook-form";
+import {useForm as formBox} from "react-hook-form";
 import Button from "@Components/Shared/Buttons/Button";
 import BtnLink from "@Components/Shared/Buttons/Link";
-const CourseRateModal = () => {
-  const {
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = formBox();
-  const onSubmit = (data) => {};
-  return (
-    <form className="CourseRateModal__form" onSubmit={handleSubmit(onSubmit)}>
-      <div className="CourseRateModal__form--title">
-        <h4>به این دوره امتیاز دهید</h4>
-      </div>
-      <div className="CourseRateModal__form--rate">
-        <Rate
-          // character={<RateIcon />}
-          allowHalf
-        />
-      </div>
-      <Textarea label="نظر شما:" name="des" control={control} errors={errors} />
-      <div className="CourseRateModal__form--btnbox">
-        <BtnLink to="/">بعدا</BtnLink>
-        <Button type="primary">ثبت</Button>
-      </div>
-    </form>
-  );
+import useFetch from "../../../../Context/useFetch";
+import {useNavigate} from "react-router";
+import {toast} from "react-toastify";
+
+
+const CourseRateModal = ({selectedCourse}) => {
+    const {
+        handleSubmit,
+        control,
+        formState: {errors},
+    } = formBox();
+
+    const navigate = useNavigate()
+    const [postPassData, setPostPassData] = useState(null);
+
+    const postScore = useFetch({
+        url: "MemberShipService",
+        method: "POST",
+        trigger: false,
+        noHeader: false,
+        data: postPassData,
+        message: 'نظر شما با موفقیت ثبت شد',
+        argFunc: () => {
+            navigate('/')
+        },
+    })
+
+    const [rate, setRate] = useState(2.5);
+    const onSubmit = (data) => {
+        let formData = new FormData();
+        formData.append("course_uuid", selectedCourse);
+        formData.append("grade", rate);
+        formData.append("text", data.text);
+        setPostPassData(formData);
+        postScore.reFetch()
+    };
+
+    return (
+        <form className="CourseRateModal__form" onSubmit={handleSubmit(onSubmit)}>
+            <div className="CourseRateModal__form--title">
+                <h4>به این دوره امتیاز دهید</h4>
+            </div>
+            <div className="CourseRateModal__form--rate">
+                <Rate
+                    // character={<RateIcon />}
+                    allowHalf
+                    value={rate}
+                    min={1}
+                    onChange={(e) => setRate(e)}
+                />
+            </div>
+            <Textarea label="نظر شما:"
+                      name="text"
+                      control={control}
+                      errors={errors}/>
+            <div className="CourseRateModal__form--btnbox">
+                <BtnLink to="/">بعدا</BtnLink>
+                <Button type="primary" htmlType="submit">ثبت</Button>
+            </div>
+        </form>
+    );
 };
 
 export default CourseRateModal;
