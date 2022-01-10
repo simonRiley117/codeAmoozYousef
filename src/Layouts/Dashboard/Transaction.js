49e169fcd0926409e97659f5ce6d4fc2406f0f3c
pt-ubuntu-os <persian1379pr@gmail.com>
2022-01-10 23:08:00 +0330

    user Transactions integrated

--- src/Layouts/Dashboard/Transaction.js
+++ src/Layouts/Dashboard/Transaction.js
@@ -1,75 +1,98 @@
-import React from "react";
+import React, {useState} from "react";
 import BreadCrump from "@Components/Shared/BreadCrump/BreadCrump";
-import { Table } from "antd";
+import {Table} from "antd";
+import useFetch from "../../Context/useFetch";
 
 function Transaction() {
-  return (
-    <div className="Transaction">
-      <p className="Transaction__Title">تراکنش ها</p>
-      <div className="Transaction__box">
-        <Table pagination={false} columns={columns} dataSource={dataSource} />
-      </div>
-    </div>
-  );
+    const [transactionData, setTransactionData] = useState(null);
+    const [transactionLoading, setTransactionLoading] = useState(true);
+    // const [dataSource, setDataSource] = useState([])
+
+    const setData = (data) => {
+        let new_data = data.results.map((item, index) => ({...item, column: ++index}))
+        console.log('new_data: ', new_data)
+        setTransactionData(new_data);
+        setTransactionLoading(false);
+    };
+
+    const getuserInfo = useFetch({
+        url: `CartService/transactions`,
+        trigger: true,
+        noHeader: false,
+        method: "GET",
+        setter: setData,
+    });
+
+    console.log('transactionData: ', transactionData)
+    return (
+        !transactionLoading ? (
+            <div className="Transaction">
+                <p className="Transaction__Title">تراکنش ها</p>
+                <div className="Transaction__box">
+                    <Table pagination={false} columns={columns} dataSource={transactionData}/>
+                </div>
+            </div>
+        ) : null
+    );
 }
 
 export default Transaction;
 const columns = [
-  {
-    title: "ردیف",
-    dataIndex: "column",
-    key: "ردیف",
-  },
-  {
-    title: "دوره",
-    dataIndex: "course",
-    key: "دوره",
-  },
-  {
-    title: "نام استاد",
-    dataIndex: "master",
-    key: "نام استاد",
-  },
-  {
-    title: "نوع مدرک",
-    dataIndex: "degree",
-    key: "نوع مدرک",
-  },
-  {
-    title: "میزان تخفیف",
-    dataIndex: "discount",
-    key: "میزان تخفیف",
-  },
-  {
-    title: "پرداختی شما",
-    dataIndex: "peyment",
-    key: "پرداختی شما",
-  },
-  {
-    title: "تاریخ پرداخت",
-    dataIndex: "date",
-    key: "تاریخ پرداخت",
-  },
+    {
+        title: "ردیف",
+        dataIndex: "column",
+        key: "ردیف",
+    },
+    {
+        title: "دوره",
+        dataIndex: "course_name",
+        key: "دوره",
+    },
+    {
+        title: "نام استاد",
+        dataIndex: "teacher_name",
+        key: "نام استاد",
+    },
+    {
+        title: "نوع مدرک",
+        dataIndex: "degree_name",
+        key: "نوع مدرک",
+    },
+    {
+        title: "میزان تخفیف",
+        dataIndex: "discount_amount",
+        key: "میزان تخفیف",
+    },
+    {
+        title: "پرداختی شما",
+        dataIndex: "discounted_cost",
+        key: "پرداختی شما",
+    },
+    {
+        title: "تاریخ پرداخت",
+        dataIndex: "date_ordered",
+        key: "تاریخ پرداخت",
+    },
 ];
-const dataSource = [
-  {
-    key: "1",
-    column: "1",
-    course: "دوره برنامه نویسی python",
-    master: "علیرضا میرزایی فرد",
-    degree: "مدرک دانشگاه صنعتی شریف",
-    discount: "100.000".toLocaleString(),
-    peyment: "400.000".toLocaleString(),
-    date: "1400 / 05 / 05",
-  },
-  {
-    key: "1",
-    column: "1",
-    course: "دوره برنامه نویسی python",
-    master: "علیرضا میرزایی فرد",
-    degree: "مدرک دانشگاه صنعتی شریف",
-    discount: "100.000".toLocaleString(),
-    peyment: "400.000".toLocaleString(),
-    date: "1400 / 05 / 05",
-  },
-];
+// const dataSource = [
+//     {
+//         key: "1",
+//         column: "1",
+//         course: "دوره برنامه نویسی python",
+//         master: "علیرضا میرزایی فرد",
+//         degree: "مدرک دانشگاه صنعتی شریف",
+//         discount: "100.000".toLocaleString(),
+//         peyment: "400.000".toLocaleString(),
+//         date: "1400 / 05 / 05",
+//     },
+//     {
+//         key: "1",
+//         column: "1",
+//         course: "دوره برنامه نویسی python",
+//         master: "علیرضا میرزایی فرد",
+//         degree: "مدرک دانشگاه صنعتی شریف",
+//         discount: "100.000".toLocaleString(),
+//         peyment: "400.000".toLocaleString(),
+//         date: "1400 / 05 / 05",
+//     },
+// ];
