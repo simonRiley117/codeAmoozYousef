5803c6156f0938b6943c0500d8817bc1db12d063
pt-ubuntu-os <persian1379pr@gmail.com>
2022-01-25 22:54:18 +0330

    feat(social auth)

--- src/Components/Layouts/Register/SignUp.js
+++ src/Components/Layouts/Register/SignUp.js
@@ -6,47 +6,67 @@ import Link from "@Components/Shared/Buttons/Link";
 
 // Assets
 import GoogleLogoPic from "@Assets/Pic/google_logo.png";
-import { ReactComponent as ExitIcon } from "@Assets/Icons/Exit.svg";
-import { ReactComponent as LinkedInIcon } from "@Assets/Icons/linkedin.svg";
-import { ReactComponent as GithubIcon } from "@Assets/Icons/github.svg";
+import {ReactComponent as ExitIcon} from "@Assets/Icons/Exit.svg";
+import {ReactComponent as LinkedInIcon} from "@Assets/Icons/linkedin.svg";
+import {ReactComponent as GithubIcon} from "@Assets/Icons/github.svg";
 import SignUpForm from "./SignUpForm";
-import { Divider } from "antd";
+import {Divider} from "antd";
+import {API_URL} from "../../../constants";
 
-const SignUp = ({ onCancel, handleActive }) => {
-  return (
-    <div className="register__form signUp">
-      <IconBtn title="بستن" icon={<ExitIcon />} onClick={onCancel} />
+const SignUp = ({onCancel, handleActive}) => {
+    return (
+        <div className="register__form signUp">
+            <IconBtn title="بستن" icon={<ExitIcon/>} onClick={onCancel}/>
 
-      <div className="register__form--wrapper signUp">
-        <h2>ثبت نام</h2>
-        {/* <div className="register__form--actions mt-6">
-          <Link
-            id="google"
-            type="primary"
-            icon={<img src={GoogleLogoPic} alt="google" />}
-            to='/'
-          >
-            ثبت نام با اکانت گوگل
-          </Link>
-          <Link to='/' type="primary" classes="social">
-            <LinkedInIcon />
-          </Link>
-          <Link to='/' type="primary" classes="social">
-            <GithubIcon />
-          </Link>
-        </div> */}
-        {/* <Divider>یا</Divider> */}
+            <div className="register__form--wrapper signUp">
+                <h2>ثبت نام</h2>
+                <div className="register__form--actions mt-6">
+                    {/*<Link*/}
+                    {/*    id="google"*/}
+                    {/*    type="primary"*/}
+                    {/*    icon={<img src={GoogleLogoPic} alt="google"/>}*/}
+                    {/*    to='/'*/}
+                    {/*>*/}
+                    {/*    ثبت نام با اکانت گوگل*/}
+                    {/*</Link>*/}
+                    <a
+                        id="google"
+                        type="primary"
+                        href={`${API_URL}/social-auth/login/google-oauth2/`}>
+                        ورود با اکانت گوگل <img src={GoogleLogoPic} alt="google"/>
+                    </a>
 
-        <SignUpForm handleActive={handleActive} />
-        <Button
-          type="text"
-          onClick={handleActive}
-          classes="register__form--other"
-        >
-          قبلا ثبت نام کردم
-        </Button>
-      </div>
-    </div>
-  );
+
+                    {/*<Link to='/' type="primary" classes="social">*/}
+                    {/*    <LinkedInIcon/>*/}
+                    {/*</Link>*/}
+                    <a href={`${API_URL}/social-auth/login/linkedin-oauth2/`}
+                       type="primary"
+                       className="social">
+                        <LinkedInIcon/>
+                    </a>
+
+                    {/*<Link to='/' type="primary" classes="social">*/}
+                    {/*    <GithubIcon/>*/}
+                    {/*</Link>*/}
+                    <a href={`${API_URL}/social-auth/login/github/`}
+                       type="primary"
+                       className="social">
+                        <GithubIcon/>
+                    </a>
+                </div>
+                <Divider>یا</Divider>
+
+                <SignUpForm handleActive={handleActive}/>
+                <Button
+                    type="text"
+                    onClick={handleActive}
+                    classes="register__form--other"
+                >
+                    قبلا ثبت نام کردم
+                </Button>
+            </div>
+        </div>
+    );
 };
 export default SignUp;
--- src/reducers/AuthReducer.js
+++ src/reducers/AuthReducer.js
@@ -1,19 +1,18 @@
-import { HOME_URL } from "@App/constants";
+import {HOME_URL} from "@App/constants";
 import Cookies from "js-cookie";
 
 export const authReducer = (state, action) => {
-  switch (action.type) {
-    case "LOGIN":
-      // localStorage.setItem("token", action.token);
-      Cookies.set("token", action.token, { domain: HOME_URL ,secure: true });
+    switch (action.type) {
+        case "LOGIN":
+            // localStorage.setItem("token", action.token);
+            Cookies.set("token", action.token, {domain: HOME_URL, secure: true});
+            return action.token;
+        case "LOGOUT":
+            // localStorage.removeItem("token");
+            Cookies.remove("token", {path: "/", domain: HOME_URL});
 
-      return action.token;
-    case "LOGOUT":
-      // localStorage.removeItem("token");
-      Cookies.remove("token", { path: "/", domain: HOME_URL });
-
-      return null;
-    default:
-      return state;
-  }
+            return null;
+        default:
+            return state;
+    }
 };
