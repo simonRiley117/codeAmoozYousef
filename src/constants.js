5fd7314078dd6937c0f432f6217be6aaca0eb15e
Saba Beigi <beigisaba@gmail.com>
2022-01-29 11:39:35 +0330

    API_URL

--- src/constants.js
+++ src/constants.js
@@ -1,15 +1,12 @@
-
- // export const API_URL = "http://88.218.17.104:9000";
+// export const API_URL = "http://88.218.17.104:9000";
 export const API_URL = "http://127.0.0.1:8000";
+// export const API_URL = "http://88.218.17.104:9000";
 export const TEAChER_URL = "http://localhost:3001";
 export const HOME_URL = "localhost";
 
 // export const API_URL = "https://finalapi.codeamooz.com";
 // export const TEAChER_URL = "https://teacher.codeamooz.com";
 // export const HOME_URL = "codeamooz.com";
-
-
-
 
 export const TOKEN =
   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjMyNTc4NDUwLCJqdGkiOiJjN2ZjYmViZDI4NjY0ODlhOTZkMzUzNTE1ZTVlZDNmMyIsInVzZXJfdXNlcm5hbWUiOiJyb290In0.tfeUIAhtZclK2YO82uH2s-mOV62vBfIkh2TuETtS3t0";
