5b8bc452a93cb1b7bcfc10132bb1fbc2dc7a108d
Saba Beigi <beigisaba@gmail.com>
2022-02-13 02:42:26 +0330

    container

--- src/Components/Shared/Menu/ProfileMenu/Propfile.js
+++ src/Components/Shared/Menu/ProfileMenu/Propfile.js
@@ -1,77 +1,72 @@
-import { Avatar, Badge, Dropdown } from 'antd';
-import React, { useState } from 'react';
+import { Avatar, Badge, Dropdown } from "antd";
+import React, { useState } from "react";
 // import { FaChevronDown } from "react-icons/fa";
-import ProfileMenu from './ProfileMenu';
-import Modal from '@Components/Shared/Modal/Modal';
-import Button from '../../Buttons/Button';
-import { useAuth } from '@App/Context/authContext';
-import useMediaQuery from '@App/Hooks/useMediaQuery';
-import { Link, useNavigate } from 'react-router-dom';
+import ProfileMenu from "./ProfileMenu";
+import Modal from "@Components/Shared/Modal/Modal";
+import Button from "../../Buttons/Button";
+import { useAuth } from "@App/Context/authContext";
+import useMediaQuery from "@App/Hooks/useMediaQuery";
+import { Link, useNavigate } from "react-router-dom";
 
 // image
-import { ReactComponent as ArrowDownIcon } from '@Assets/Icons/arrow-down.svg';
-import { useUserData } from '@App/Context/userContext';
+import { ReactComponent as ArrowDownIcon } from "@Assets/Icons/arrow-down.svg";
+import { useUserData } from "@App/Context/userContext";
 
 const Propfile = () => {
-	const { userData } = useUserData();
-	const [modal, setModal] = useState(false);
+  const { userData } = useUserData();
+  const [modal, setModal] = useState(false);
 
+  let navigate = useNavigate();
+  const { authDispatch } = useAuth();
 
-	let navigate = useNavigate();
-	const { authDispatch } = useAuth();
+  const handleModalShow = () => {
+    setModal((prev) => !prev);
+  };
 
-	const handleModalShow = () => {
-		setModal((prev) => !prev);
-	};
-
-	const logout = () => {
-		authDispatch({ type: 'LOGOUT' });
-		navigate('/', { replace: true });
-	};
-	const Icon = (
-		<span>
-			<ArrowDownIcon />
-		</span>
-	);
-	return (
-		<>
-			<Dropdown
-				arrow
-				trigger={['click', 'hover']}
-				overlay={<ProfileMenu handleModalShow={handleModalShow} />}
-				getPopupContainer={(triggerNode) => triggerNode.parentNode}
-				destroyPopupOnHide
-			>
-				<div className='appbar__profile'>
-					<Badge count={Icon}>
-						<Avatar src={userData.cover} alt='name' shape='square'>
-							{userData.username}
-						</Avatar>
-					</Badge>
-				</div>
-			</Dropdown>
-			<Modal
-				className='ExitModal'
-				visible={modal}
-				onCancel={handleModalShow}
-			>
-				<div className='ExitModal__back'>
-					<p className='mb-12'>آیا از خروج مطمئن هستید؟</p>
-					<div className='d-flex-space'>
-						<Button onClick={logout}>بله</Button>
-						<Button onClick={handleModalShow} type='primary'>
-							خیر
-						</Button>
-					</div>
-				</div>
-			</Modal>
-		</>
-	);
+  const logout = () => {
+    authDispatch({ type: "LOGOUT" });
+    navigate("/", { replace: true });
+  };
+  const Icon = (
+    <span>
+      <ArrowDownIcon />
+    </span>
+  );
+  return (
+    <>
+      <Dropdown
+        arrow
+        trigger={["hover", "click"]}
+        overlay={<ProfileMenu handleModalShow={handleModalShow} />}
+        getPopupContainer={(triggerNode) => triggerNode.parentNode}
+        destroyPopupOnHide
+      >
+        <div className="appbar__profile">
+          <Badge count={Icon}>
+            <Avatar src={userData.cover} alt="name" shape="square">
+              {userData.username}
+            </Avatar>
+          </Badge>
+        </div>
+      </Dropdown>
+      <Modal className="ExitModal" visible={modal} onCancel={handleModalShow}>
+        <div className="ExitModal__back">
+          <p className="mb-12">آیا از خروج مطمئن هستید؟</p>
+          <div className="d-flex-space">
+            <Button onClick={logout}>بله</Button>
+            <Button onClick={handleModalShow} type="primary">
+              خیر
+            </Button>
+          </div>
+        </div>
+      </Modal>
+    </>
+  );
 };
 export default Propfile;
 
 {
-	/* {isTablet ? (
+  /* {isTablet ? (
 				<div className='appbar__profile--area'>
 					<div className='flex gap-x-4 '>
 						<div className='appbar__profile'>
