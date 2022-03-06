ee54e23cebd23b1bfe27b949781bb3bf6a6693fa
Saba Beigi <beigisaba@gmail.com>
2022-03-06 04:14:36 +0330

    fix search

--- src/Layouts/Courses/Searchxx.js
+++ src/Layouts/Courses/Searchxx.js
@@ -1,82 +1,94 @@
-import React, {useState} from "react";
-import {useForm} from "react-hook-form";
+import React, { useState } from "react";
+import { useController, useForm } from "react-hook-form";
 import Input from "../../Components/Shared/Inputs/Input";
-import {ReactComponent as UserIcon} from "@Assets/Icons/fe_search.svg";
-import {searchItem} from "@App/Recoil/StateRecoil";
-import {useRecoilState} from "recoil";
-import {createSearchParams, useNavigate} from "react-router-dom";
+import { ReactComponent as UserIcon } from "@Assets/Icons/fe_search.svg";
+import { searchItem } from "@App/Recoil/StateRecoil";
+import { useRecoilState } from "recoil";
+import { createSearchParams, useNavigate } from "react-router-dom";
 import searchic from "@Assets/Pic/search.png";
 import Vector from "@Assets/Pic/Vector.png";
 import search from "@Assets/Pic/fe_search.png";
+// import { Input } from "antd";
+// const { Search } = Input;
 
-const Searchxx = ({children}) => {
-    const [text, setText] = useRecoilState(searchItem);
-    const [result, setResult] = useState(false);
-    const navigate = useNavigate();
-    const {
-        handleSubmit,
-        control,
-        reset,
-        formState: {errors},
-    } = useForm();
-    const onSubmit = ({search}) => {
+const Searchxx = ({ children }) => {
+  const [text, setText] = useRecoilState(searchItem);
 
-        // window.location.href = `/search/${data.search}`;
-        // console.log('searchXXX: ', search)
-        navigate({
-            pathname: "/search",
-            search: `?${createSearchParams({s: search})}`,
-            // search: value,
-        });
-        reset();
-    };
-    return (
-        <form onSubmit={handleSubmit(onSubmit)} className="search-container">
-            <Input
-                message="جستجو دوره"
-                name="search"
-                control={control}
-                errors={errors}
-                classes="search"
-                placeholder="میخوای چی یاد بگیری"
-                suffix={
-                    <UserIcon
-                        onClick={handleSubmit(onSubmit)}
-                        className="cursor-pointer"
-                    />
-                }
-                // onChange={(e) => {setResult(true);}}
-                // onblur={() => setResult(false)}
-            />
-            {/* <img
+  const [result, setResult] = useState(false);
+  const navigate = useNavigate();
+  const {
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const { field } = useController({
+    name: "search",
+    control,
+  });
+  const onSubmit = ({ search }) => {
+    // window.location.href = `/search/${data.search}`;
+    // console.log('searchXXX: ', search)
+    console.log("text", search);
+
+    navigate({
+      pathname: "/search",
+      search: `?${createSearchParams({ s: search })}`,
+      // search: value,
+    });
+    reset();
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="search-container">
+      <Input
+        message="جستجو دوره"
+        name="search"
+        control={control}
+        errors={errors}
+        classes="search"
+        placeholder="میخوای چی یاد بگیری"
+        suffix={
+          <UserIcon
+            onClick={handleSubmit(onSubmit)}
+            className="cursor-pointer"
+          />
+        }
+        {...field}
+        // onChange={(e) => {
+        //   setResult(true);
+        //   setText(e.target.value);
+        // }}
+        // onblur={() => setResult(false)}
+      />
+      {/* <img
         src={Magnifier}
         alt="magnifier"
         className="search-img cursor-pointer"
         
       /> */}
-            {/*{result && (*/}
-            {/*    <div className="Search__boxDisplay">*/}
-            {/*        <div className="grid grid-cols-2 gap-8 ">*/}
-            {/*            <div className="Search__contentBox flex items-center justify-start">*/}
-            {/*                <img src={searchic} alt="magnifier" className=" cursor-pointer"/>*/}
-            {/*                <p>aaa</p>*/}
-            {/*            </div>*/}
-            {/*            <div className="Search__contentBox flex items-center justify-start">*/}
-            {/*                <img src={searchic} alt="magnifier" className=" cursor-pointer"/>*/}
-            {/*                <p>aaa</p>*/}
-            {/*            </div>*/}
-            {/*        </div>*/}
-            {/*        <div className="flex items-center justify-between mt-5 Search__footer">*/}
-            {/*            <div className="flex items-center Search__lastsearches">*/}
-            {/*                <img src={Vector} alt={Vector}/>*/}
-            {/*                <p>aaa</p>*/}
-            {/*            </div>*/}
-            {/*            <img src={search} alt={search}/>*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*)}*/}
-        </form>
-    );
+      {/* {result && (
+        <div className="Search__boxDisplay">
+          <div className="grid grid-cols-2 gap-8 ">
+            <div className="Search__contentBox flex items-center justify-start">
+              <img src={searchic} alt="magnifier" className=" cursor-pointer" />
+              <p>{text}</p>
+            </div>
+            <div className="Search__contentBox flex items-center justify-start">
+              <img src={searchic} alt="magnifier" className=" cursor-pointer" />
+              <p>{text}</p>
+            </div>
+          </div>
+          <div className="flex items-center justify-between mt-5 Search__footer">
+            <div className="flex items-center Search__lastsearches">
+              <img src={Vector} alt={Vector} />
+              <p>{text}</p>
+            </div>
+            <img src={search} alt={search} />
+          </div>
+        </div>
+      )} */}
+    </form>
+  );
 };
 
 export default Searchxx;
--- src/Layouts/Search/Search.js
+++ src/Layouts/Search/Search.js
@@ -6,6 +6,7 @@ import Pagination from "@Components/Shared/Pagination";
 import useFetch from "@App/Context/useFetch";
 import Coursecardsm from "@Components/Layouts/Course/Cards/CourseCardSm";
 import Searchxx from "../Courses/Searchxx";
+import { Skeleton } from "antd";
 
 function Search() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -23,46 +24,69 @@ function Search() {
   useEffect(() => {
     getallCourseList.reFetch();
   }, [searchParams]);
-console.log('allcourse', allcourse?.results)
+  console.log("allcourse", allcourse?.results);
   const PageSize = 11;
   const [currentPage, setCurrentPage] = useState(1);
   return (
     <div className="Search container">
-      <div
-        className="courses Search__box flex-column"
-        style={{ paddingTop: "10rem" }}
-      >
-        <div className="Search__input">
-          <Searchxx />
-        </div>
-        <p className="Courses__searchTitle font-bold flex-column">
-          {allcourse?.results.length > 0 ? (
-            <> نتیجه جستجو شما برای "{searchParams.get("s")}":</>
+      {getallCourseList.response?.data ? (
+        <div
+          className="courses Search__box flex-column"
+          style={{ paddingTop: "10rem" }}
+        >
+          <div className="Search__input">
+            <Searchxx />
+          </div>
+          <p className="Courses__searchTitle font-bold flex-column">
+            {allcourse?.results.length > 0 ? (
+              <> نتیجه جستجو شما برای "{searchParams.get("s")}":</>
+            ) : (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <img src={searchNotFound} alt={searchNotFound} />
+                <p style={{ fontSize: "3rem" }}>هیچ نتیجه ای یافت نشد</p>
+              </div>
+            )}
+          </p>
+          {allcourse?.length !== 0 ? (
+            <div className=" courses__grid grid grid-cols-4 gap-x-6 gap-y-8  ">
+              {allcourse?.results.map((card) => {
+                return <Coursecardsm key={card.uuid} card={card} />;
+              })}
+            </div>
           ) : (
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <img src={searchNotFound} alt={searchNotFound} />
-              <p style={{ fontSize: "3rem" }}>هیچ نتیجه ای یافت نشد</p>
+            <div className="Courses__imgBox flex justify-center items-center">
+              <img src={searchic} alt={searchic} className="Courses__img" />
             </div>
           )}
-        </p>
-        {allcourse?.length !== 0 ? (
-          <div className=" courses__grid grid grid-cols-4 gap-x-6 gap-y-8  ">
-            {allcourse?.results.map((card) => {
-              return <Coursecardsm key={card.uuid} card={card} />;
-            })}
-          </div>
-        ) : (
-          <div className="Courses__imgBox flex justify-center items-center">
-            <img src={searchic} alt={searchic} className="Courses__img" />
-          </div>
-        )}
-      </div>
+        </div>
+      ) : (
+        <div className="w-11/12	 m-auto mt-44">
+          <Skeleton.Button block active size="large" />
+          <br />
+          <br />
+          <Skeleton.Button block active size="large" />
+          <br />
+          <br />
+          <Skeleton.Button block active size="large" />
+          <br />
+          <br />
+          <Skeleton.Button block active size="large" />
+          <br />
+          <br />
+          <Skeleton.Button block active size="large" />
+          <br />
+          <br />
+          <Skeleton.Button block active size="large" />
+          <br />
+          <br />
+        </div>
+      )}
 
       <div className="Title-paging">
         <Pagination
