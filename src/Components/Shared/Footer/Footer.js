44a8bd1fce7cd859a6863b2e00c3ac2030d05500
Masoud Mir hosseini <sm2.masoud@gmail.com>
2024-04-19 13:00:37 +0000

    Update Footer.js

--- src/Components/Shared/Footer/Footer.js
+++ src/Components/Shared/Footer/Footer.js
@@ -33,12 +33,12 @@ const Footer = () => {
   //window.open(&quot;https://logo.samandehi.ir/Verify.aspx?id=124402&p=rfthuiwkaodsaodsobpduiwk&quot;, &quot;Popup&quot;,&quot;toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=450, height=630, top=30&quot;)
   const handleClick = () => {
     window.open(
-    "https://logo.samandehi.ir/Verify.aspx?id=124402&p=rfthuiwkaodsaodsobpduiwk",
-	"popup",
-	"toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=450, height=630, top=30"
+      "https://logo.samandehi.ir/Verify.aspx?id=124402&p=rfthuiwkaodsaodsobpduiwk",
+      "popup",
+      "toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=450, height=630, top=30"
     );
   };
-  const onSubmit = data => {};
+  const onSubmit = data => { };
 
   return (
     <footer className="footer mt-48 pb-8">
@@ -52,44 +52,44 @@ const Footer = () => {
               </div>
             </Link>
             <p>
-            سامانه «کدآموز» در زمینه توسعه نرم‌افزار و برنامه‌نویسی، به عنوان یک پلتفرم تخصصی، خدمات گسترده‌ای ارائه می‌دهد که شامل آموزش و ارزیابی تعاملی می‌شود. این دوره‌ها و منابع آموزشی توسط اساتید و متخصصان با تجربه ارائه می‌شود و داشتن امکان ارزیابی تعاملی، این امکان را به دانشجویان و علاقمندان می‌دهد بتوانند در قالب آزمون و یا پروژه‌های عملی، مهارت‌های خود را محک بزنند.  
+              سامانه «کدآموز» در زمینه توسعه نرم‌افزار و برنامه‌نویسی، به عنوان یک پلتفرم تخصصی، خدمات گسترده‌ای ارائه می‌دهد که شامل آموزش و ارزیابی تعاملی می‌شود. این دوره‌ها و منابع آموزشی توسط اساتید و متخصصان با تجربه ارائه می‌شود و داشتن امکان ارزیابی تعاملی، این امکان را به دانشجویان و علاقمندان می‌دهد بتوانند در قالب آزمون و یا پروژه‌های عملی، مهارت‌های خود را محک بزنند.
 
 
             </p>
             <div className="flex gap-5 footer__info--symbols mt-6">
-			<a
-                        referrerPolicy="origin"
-                        target="_blank"
-                        href="https://trustseal.enamad.ir/?id=251667&amp;Code=ORS8gimDMkFKmWVzad3T"
-                    >
-                        <img
-                            className="h-40"
-                            referrerPolicy="origin"
-                            src="https://Trustseal.eNamad.ir/logo.aspx?id=251667&amp;Code=ORS8gimDMkFKmWVzad3T"
-                            alt=""
-                            id="ORS8gimDMkFKmWVzad3T"
-                        />
-                    </a>
-                    <img
-                        onClick={handleClick}
-                        className="cursor-pointer h-40"
-                        referrerPolicy="origin"
-                        id="jxlznbqewlaowlaoesgtnbqe"
-                        alt="logo-samandehi"
-                        src="https://logo.samandehi.ir/logo.aspx?id=124402&p=nbpdodrfshwlshwllymaodrf"
-                    />
+              <a
+                referrerPolicy="origin"
+                target="_blank"
+                href="https://trustseal.enamad.ir/?id=251667&amp;Code=ORS8gimDMkFKmWVzad3T"
+              >
+                <img
+                  className="h-40"
+                  referrerPolicy="origin"
+                  src="https://Trustseal.eNamad.ir/logo.aspx?id=251667&amp;Code=ORS8gimDMkFKmWVzad3T"
+                  alt=""
+                  id="ORS8gimDMkFKmWVzad3T"
+                />
+              </a>
+              <img
+                onClick={handleClick}
+                className="cursor-pointer h-40"
+                referrerPolicy="origin"
+                id="jxlznbqewlaowlaoesgtnbqe"
+                alt="logo-samandehi"
+                src="https://logo.samandehi.ir/logo.aspx?id=124402&p=nbpdodrfshwlshwllymaodrf"
+              />
             </div>
           </section>
           <section className="footer__about ">
             <h3 className="footer__title">تماس با ما</h3>
             <p className="mb-4">
-            آدرس دفتر: اصفهان – بلوار دانشگاه صنعتی اصفهان – شهرک علمی و تحقیقاتی اصفهان ساختمان ابوریحان – واحد 135
+              آدرس دفتر: اصفهان – بلوار دانشگاه صنعتی اصفهان – شهرک علمی و تحقیقاتی اصفهان ساختمان ابوریحان – واحد 135
 
             </p>
             <div className="flex items-center justify-between mb-4">
               <span>ساعات تماس:</span>
               <span>شنبه تا چهارشنبه از ساعت 9 الی 18
-</span>
+              </span>
             </div>
             <div className="flex items-center justify-between mb-4">
               <span>تلفن:</span>
@@ -108,9 +108,9 @@ const Footer = () => {
             <h3 className="footer__title"></h3>
             <Link to="/about-me">درباره ما</Link>
             <Link to="/contact-us">تماس با ما</Link>
-            <Link to="/">قوانین و مقررات</Link>
-           
-            <Link to="/">سوالات متداول</Link>
+            // <Link to="/faq">قوانین و مقررات</Link>
+
+            <Link to="/faq">سوالات متداول</Link>
           </section>
           <section className="footer__contact  flex items-center justify-center flex-col">
             <h4>ارتباط با ما</h4>
@@ -125,7 +125,7 @@ const Footer = () => {
                 }}
                 name="email"
                 control={control}
-                // value={teacherCoworkerData?.first_name}
+              // value={teacherCoworkerData?.first_name}
               />
               <TextArea
                 label="پیام شما"
@@ -139,12 +139,12 @@ const Footer = () => {
                 control={control}
                 minRows={3}
                 maxRows={3}
-                // value={teacherCoworkerData?.bio}
+              // value={teacherCoworkerData?.bio}
               />
               <Button
                 type="primary"
                 htmlType="submit"
-                // disabled={acceptRules ? false : true}
+              // disabled={acceptRules ? false : true}
               >
                 ارسال
               </Button>
@@ -152,18 +152,25 @@ const Footer = () => {
           </section>
         </div>
         <section className="footer__social flex justify-end gap-x-7">
-          <span className="footer__social--item">
-            <TelegramIcon />
-          </span>
-          <span className="footer__social--item">
-            <WhatsappIcon />
-          </span>
-          <span className="footer__social--item">
-            <InstagramIcon />
-          </span>
-          <span className="footer__social--item">
-            <LinkedInIcon />
-          </span>
+          <a target='_blank' href="">
+            <span className="footer__social--item">
+              <TelegramIcon />
+            </span></a>
+          <a target='_blank' href="https://wa.me/">
+            <span className="footer__social--item">
+              <WhatsappIcon />
+            </span>
+          </a>
+          <a target='_blank' href="https://www.instagram.com/codeamooz_co?igsh=YnVnbXFmc3hpbjl">
+            <span className="footer__social--item">
+              <InstagramIcon />
+            </span>
+          </a>
+          <a target='_blank' href="https://www.linkedin.com/company/codeamooz/">
+            <span className="footer__social--item">
+              <LinkedInIcon />
+            </span>
+          </a>
         </section>
       </div>
     </footer>
