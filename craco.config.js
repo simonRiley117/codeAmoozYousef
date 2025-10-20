f66a168eb33be3e483dc7c1209257d36d2b248dc
yousefamm <yousefemami548@gmail.com>
2025-10-20 16:06:16 +0330

    better

--- craco.config.js
+++ craco.config.js
@@ -1,40 +1,21 @@
-let CracoLessPlugin = require("craco-less");
-const CracoAlias = require("craco-alias");
-
 module.exports = {
-  style: {
-    postcss: {
-      plugins: [require("tailwindcss"), require("autoprefixer")],
+  webpack: {
+    configure: (webpackConfig) => {
+      // اضافه کردن rule برای فایل‌های LESS
+      webpackConfig.module.rules.push({
+        test: /\.less$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: { javascriptEnabled: true },
+            },
+          },
+        ],
+      });
+      return webpackConfig;
     },
   },
-  plugins: [
-    {
-      plugin: CracoLessPlugin,
-      options: {
-        lessLoaderOptions: {
-          lessOptions: {
-            modifyVars: {
-              "@primary-color": "#F68521",
-              "@success-color": "#329C00",
-              "@error-color": "#FF5A67",
-              "@highlight-color": "#FF5A67",
-              "@text-color": "#121212",
-              "@text-color-secondary": "#121212B2",
-              "@border-color-base": "#6D6D6D",
-              "@border-radius-base": "1rem",
-            },
-
-            javascriptEnabled: true,
-          },
-        },
-      },
-    },
-    {
-      plugin: CracoAlias,
-      options: {
-        source: "jsconfig",
-        baseUrl: "./",
-      },
-    },
-  ],
 };
